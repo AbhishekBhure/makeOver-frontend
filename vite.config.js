3d6edbcdfdc2504b739b2d64e7065d166a1ea335
abhi <abhiab7411@gmail.com>
2024-01-14 13:57:36 +0530

    modified: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "https://makeover-backend.onrender.com",
-        secure: false,
-      },
-    },
-  },
+  server: {},
   plugins: [react()],
 });
